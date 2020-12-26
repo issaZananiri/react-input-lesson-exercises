6f46dc2822767503204f9fa517e8ea06349695c1
issaZananiri <https://github.com/issaZananiri>
2020-12-26 16:38:33 +0200

    done

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -10,15 +10,28 @@ class Exercise1 extends Component {
         }
     }
 
+
+    showAlert = () => {
+        alert(`Come in ${this.state.name}, you're ${this.state.age} - that's good enough`)
+    }
+
+
+    handleInputChange = event => {
+        const {value, name} = event.target;    
+        this.setState({
+          [name]: value
+        });
+    }
+
+
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" name="name" onChange={this.handleInputChange}/>
+                <input id="age-input" name="age" onChange={this.handleInputChange}/>
+                <button onClick={this.showAlert}>Go to Bar</button>
             </div>
         );
     }
 }
-
 export default Exercise1;
